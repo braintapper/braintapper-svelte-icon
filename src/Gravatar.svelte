95f0cbbe9640a654ea1f7e6d992effc7e1530af2
Steven Ng <steven.ng@braintapper.com>
2021-08-10 11:06:43 -0400

    Revamp gravatar

--- src/Gravatar.svelte
+++ src/Gravatar.svelte
@@ -1,12 +1,14 @@
 <script>
-  export let gravatarHash = undefined;
+  import md5 from "md5";
+  export let email = undefined;
   export let altText = "Gravatar";
   export let style = undefined;
   export let id = undefined;
   export let classes = undefined;
+  $: gravatarHash = md5(email);
 </script>
 
-{#if gravatarHash}
+{#if email}
   <img src="https://www.gravatar.com/avatar/{gravatarHash || ""}" {style} {id} class={classes} alt={altText}/>
 {:else}
   <img src="https://www.gravatar.com/avatar/unknown" {style} {id} class={classes} alt={altText}/>
