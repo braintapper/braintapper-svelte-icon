46eb466bde25c4af26cc361237675eef6f5c8954
Steven Ng <steven.ng@braintapper.com>
2021-08-11 10:04:47 -0400

    Add fill to SVG, minor updates, version bump

--- src/Gravatar.svelte
+++ src/Gravatar.svelte
@@ -1,15 +1,21 @@
 <script>
   import md5 from "blueimp-md5-es6/js/md5.js";
   export let email = undefined;
+  export let alternateDefault = undefined;
   export let altText = "Gravatar";
   export let style = undefined;
   export let id = undefined;
   export let classes = undefined;
+  let gravatarHash = "";
   $: gravatarHash = md5(email);
 </script>
 
 {#if email}
   <img src="https://www.gravatar.com/avatar/{gravatarHash || ""}" {style} {id} class={classes} alt={altText}/>
 {:else}
-  <img src="https://www.gravatar.com/avatar/unknown" {style} {id} class={classes} alt={altText}/>
+  {if alternateDefault}
+    <img src="https://www.gravatar.com/avatar/unknown" {style} {id} class={classes} alt={altText}/>
+  {:else}
+    <img src="https://www.gravatar.com/avatar/unknown" {style} {id} class={classes} alt={altText}/>
+  {/if}
 {/if}
--- src/Svg.svelte
+++ src/Svg.svelte
@@ -7,8 +7,9 @@
   export let icon = "empty"; // just an empty square, can be placeholder
   export let classes = undefined;
   export let style = undefined;
+  export let fill = undefined;
 
 </script>
 
 
-<svg xmlns="http://www.w3.org/2000/svg" width="{size}" height="{size}" viewBox="0 0 24 24" {id} {style} class={classes}><path d={icons[icon]}/></svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="{size}" height="{size}" viewBox="0 0 24 24" {id} {style} class={classes}><path d={icons[icon]} {fill}/></svg>
